8c798d265c967b2bc67469e9d01345a10027f674
kimbriel21 <kimbriel21@gmail.com>
2018-03-14 13:49:06 +0800

    done

--- resources/views/view_image.blade.php
+++ resources/views/view_image.blade.php
@@ -1,3 +1,11 @@
-<div class="" style='background-image: url("{{$img_url}}"); background-size: cover; height: 100vh'>
-	
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="" style='background-image: height: 100vh'>
+	<img src="{{$img_url}}" alt="Mountain View" style="height: 100vh;">
 </div>
+</body>
+</html>
