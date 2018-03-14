e1f4c9edf65adda8114aecdc94fe718132871407
kimbriel21 <kimbriel21@gmail.com>
2018-03-14 13:53:58 +0800

    done

--- resources/views/view_image.blade.php
+++ resources/views/view_image.blade.php
@@ -4,8 +4,8 @@
 	<title></title>
 </head>
 <body>
-<div class="" style='background-image: height: 100vh'>
-	<img src="{{$img_url}}" alt="Mountain View" style="height: 100vh;">
+<div class="" style='background-image: height: 100vh; background-size: cover; object-fit: cover;'>
+	<img src="{{$img_url}}" alt="Mountain View" style="height: 100%; width: 100%; ">
 </div>
 </body>
 </html>
